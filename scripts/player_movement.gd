f9d5fa66ea666c1ff0cee6fd5d6f5dfa7feff2ff
deanxox <shidian324@gmail.com>
2024-10-18 23:18:48 +0200

    Some work on scene switching

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -1,9 +1,10 @@
 extends CharacterBody2D
 
-const run_speed := 35.0
+const run_speed := 60.0
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Global.teleported.connect(_on_teleported)
 	pass # Replace with function body.
 
 
@@ -14,3 +15,6 @@ func _process(delta: float) -> void:
 	velocity.y += gravity * delta
 	move_and_slide()
 	pass
+
+func _on_teleported(destination):
+	position = Global.destination_coordinates[destination]
