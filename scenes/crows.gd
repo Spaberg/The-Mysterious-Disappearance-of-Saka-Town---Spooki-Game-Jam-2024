7a63727e5df4f521db1b9a75d908e38a3f17c97b
David <david@spaberg.se>
2024-10-19 12:12:14 +0200

    Lamp and crow details

--- scenes/crows.gd
+++ scenes/crows.gd
@@ -1,21 +1,25 @@
 extends AudioStreamPlayer2D
 
+var shift_range : int = 600
+var dir : int = 1
+var velocity : int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-	#position.x += 10
-	#if(position.x < player.position.x + 360):
-		#position.x = player.position.x - 360
-
+	position.x += velocity*delta
 
 func _on_timer_timeout() -> void:
+	position = Vector2(Player.position.x + (randf() - 0.5)*shift_range, Player.position.y)
+	if(randf() > 0.5):
+		dir = 1
+	else:
+		dir = -1
+	velocity = randf()*50*dir
 	play()
-	$Timer.wait_time = 8 + randf() * 15
+	$Timer.wait_time = 8 #+ randf() * 15
 	$Timer.start()
 	
--- scenes/lamp_light.gd
+++ scenes/lamp_light.gd
@@ -1,5 +1,6 @@
 extends PointLight2D
 
+var time : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,10 +9,12 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	time += 10 * delta
+	energy = 1*(sin(time)+1)*0.4
+	texture_scale = 4 + 9 * (sin(time*0.15)+1)*0.4
 
 
 func _on_flicker_timer_timeout() -> void:
 	enabled = !enabled
-	$FlickerTimer.wait_time = randf()*0.5
+	$FlickerTimer.wait_time = randf()*0.6
 	$FlickerTimer.start()
