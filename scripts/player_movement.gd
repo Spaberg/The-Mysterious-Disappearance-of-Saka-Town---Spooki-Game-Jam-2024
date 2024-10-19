bd988fe7ee1771d2e5273d9c0e7533b5eb9f7ce3
wsnordenmark <wsnordenmark@tutamail.com>
2024-10-19 23:30:28 +0200

    aoijfeoifj

--- scripts/player_movement.gd
+++ scripts/player_movement.gd
@@ -11,7 +11,7 @@ const run_speed := 25.0
 var gravity := ProjectSettings.get("physics/2d/default_gravity") as float
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_parent().position = Vector2(-1566,178)
+	get_parent().position = Vector2(-1720,290)
 	Global.teleported.connect(_on_teleported)
 	pass # Replace with function body.
 
