7a918c472fafe7162a4010abe0077c50931a5ea4
David <david@spaberg.se>
2024-10-20 12:23:12 +0200

    Demo poster in the woods

--- scripts/interaction.gd
+++ scripts/interaction.gd
@@ -1,21 +1,25 @@
 extends Node2D
 var is_collision : bool = false
 signal display_poster
-@export var poster_scene : PackedScene
+var poster_scene = preload("res://scenes/poster.tscn")
+@export var text : String
+@export var picture : CompressedTexture2D
 var poster_instance
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	poster_instance = poster_scene.instantiate()
-	pass # Replace with function body.
-
+	poster_instance.find_child("ColorRect").find_child("Text").text = "[center]" + text
+	poster_instance.find_child("ColorRect").find_child("Picture").texture = picture
 
 func _process(delta: float) -> void:
-	
 	if (Input.is_action_just_released("Interact") and is_collision and not Global.pausing):
 		Global.display_poster(poster_instance)
 	elif (Input.is_action_just_released("Interact") and is_collision and Global.pausing):
 		Global.hide_poster()
 		poster_instance = poster_scene.instantiate()
+		poster_instance.find_child("ColorRect").find_child("Text").text = "[center]" + text
+		poster_instance.find_child("ColorRect").find_child("Picture").texture = picture
 	pass
 
 func _on_area_entered(area):
